1de0f3191fd40dc1c10633457b88504d687c1fc9
ankit120695 <122710945+ankit120695@users.noreply.github.com>
2023-02-20 13:02:44 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,60 @@
+package com.demo;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int rollNo;
+    private String name;
+    private String address;
+    private String phoneNo;
+    private String degree;
+    
+    public int getRollNo() {
+        return rollNo;
+    }
+    
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+    
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+    
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+    
+    public String getDegree() {
+        return degree;
+    }
+    
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+}
